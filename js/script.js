781a3f82ab886504ee2bb2a6d4a8296fa6580005
lilo <lisabasarab@gmail.com>
2022-11-20 17:57:56 +0300

    First lesson

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Одина из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Одина из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
